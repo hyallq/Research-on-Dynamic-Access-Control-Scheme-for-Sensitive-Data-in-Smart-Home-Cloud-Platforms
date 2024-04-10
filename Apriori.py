d4649fb176b670c433999d1d1c71f2fde9fc3df5
hyallq <2324327226@qq.com>
2024-04-10 11:38:21 +0800

    fc

---
+++ Apriori.py
@@ -0,0 +1,33 @@
+import csv
+from apyori import apriori
+
+
+# 加载数据集
+def load_data(data):
+    transactions = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            transactions.append(row)
+    return transactions
+
+
+# 执行 Apriori 算法
+def run_apriori(transactions, min_support=0.5, min_confidence=0.5):
+    rules = apriori(transactions, min_support=min_support, min_confidence=min_confidence)
+    return list(rules)
+
+
+if __name__ == '__main__':
+    # 替换为你的 CSV 文件路径
+    filename = 'subset_data.csv'
+
+    # 加载数据集
+    transactions = load_data(filename)
+
+    # 执行 Apriori 算法
+    rules = run_apriori(transactions)
+
+    # 打印结果
+    for item in rules:
+        print(item)
